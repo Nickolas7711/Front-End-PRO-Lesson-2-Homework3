0b17b57089688c0f8c530bcc9b9182bf3aa88042
Mykola Kuniev <Nickolas7711@gmail.com>
2023-04-24 15:19:41 +0300

    Lesson 2/ homework 3

---
+++ index.js
@@ -0,0 +1,4 @@
+valueHours = +prompt('Enter number of hours');
+calculationSeconds = valueHours * 60 * 60;
+valueSeconds = calculationSeconds;
+alert(`In ${valueHours} hours ${valueSeconds} seconds.`)
